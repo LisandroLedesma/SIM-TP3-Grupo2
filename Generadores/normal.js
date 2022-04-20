505cc7f3102fd88aee599eb6355b2231f1f28ede
GastonCarg <gastoncargnelutti@gmail.com>
2022-04-19 21:43:13 -0300

    cambios normal

--- Generadores/normal.js
+++ Generadores/normal.js
@@ -33,17 +33,19 @@ function generarRandomsUnif(cant_n){
     return randoms
 }
 
+var rnd1 = 0;
+var rnd2 = 0;
 const distribucionNormal = (media, desviacion, i) => {
-
     //Box muller
     let z = 0;
 
-    let rnd1 = truncateDecimals(Math.random(), 4);
-    while (rnd1 === 0) {
+    if (i === 0 || i % 2 === 0) {
         rnd1 = truncateDecimals(Math.random(), 4);
+        while (rnd1 === 0) {
+            rnd1 = truncateDecimals(Math.random(), 4);
+        }
+        rnd2 = truncateDecimals(Math.random(), 4);
     }
-
-    let rnd2 = truncateDecimals(Math.random(), 4);
 
     if (i % 2 == 0) {
         z = ((Math.sqrt(-2 * Math.log(rnd1)) * Math.sin(2 * Math.PI * rnd2)) * desviacion) + media;
@@ -51,7 +53,6 @@ const distribucionNormal = (media, desviacion, i) => {
         z = ((Math.sqrt(-2 * Math.log(rnd1)) * Math.cos(2 * Math.PI * rnd2)) * desviacion) + media;
     }
 
-    // let var_aleatoria = z.toFixed(4);
     let var_aleatoria = truncateDecimals(z, 4);
 
     return var_aleatoria;
@@ -64,8 +65,6 @@ const generacionVariablesAleatoriasNormales = (media, desviacion, n) => {
     for (let i = 0; i < n; i++) {
         randObj = {
             n: i + 1,
-            // Aleatorio: Number(distribucionNormal(media, desviacion, n)),
-            // Aleatorio: Number(distribucionNormal(media, desviacion, i)),
             Aleatorio: distribucionNormal(media, desviacion, i),
         };
 
